972c6c74fabf8e7c72985d081dfd0e5dd692baa9
jwzimmer <jzimmer1@uvm.edu>
2022-02-24 16:33:01 -0500

    update text_parsing with more generalized get_likely function

--- text_parsing.py
+++ text_parsing.py
@@ -11,28 +11,35 @@ def pad_punctuation(text):
     words = words.split()
     return words
 
-def find_likely_email(text, chunk_size, flag_words = ["data","dataset","access","obtain","get","copy"]):
-    """ Takes a string of text, chunk size, and words to look for, returns a list of chunks of text around email addresses that
-    might be saying to contact this address for data, and the email addresses. """
-    pattern = re.compile(r'\b\S+@\S+\b')
+link_pattern = re.compile(r'(?<=\s|\/|:)\b[https:\/\/]*[http:\/\/]*\w+\.\S+\b')
+email_pattern = re.compile(r'\b\S+@\S+\b')
+link_flag_words = ["data","dataset","access","obtain","get","copy","go to","repo","repository","github","link"]
+email_flag_words = ["data","dataset","access","obtain","get","copy"]
+
+def find_likely(text, chunk_size, pattern, flag_words):
+    """ Takes a string of text, chunk size, regex pattern, and words to look for, returns a list of chunks of text
+    around the likely object that might be what we're looking for,
+    and the likely object itself. """
     likely_list = []
     for match in pattern.finditer(text):
         location = match.start()
-        if location < chunk_size:
+        the_match = match.group()
+        if (location + len(the_match)) < chunk_size:
             start_chunk = 0
         else:
             start_chunk = location - chunk_size
-        if len(text) - location < chunk_size:
+        if (len(text) - (location + len(the_match))) < chunk_size:
             end_chunk = len(text)
         else:
-            end_chunk = location + chunk_size
+            end_chunk = location + len(the_match) + chunk_size
         chunk = text[start_chunk:end_chunk]
         chunk_words = set(pad_punctuation(chunk))
         flag_words = set(flag_words)
         if len(list(chunk_words.intersection(flag_words))) > 0:
-            likely_list += [[match.group(), chunk]]
+            likely_list += [[the_match, chunk]]
         else:
             pass
     return likely_list
 
-print(find_likely_email("corresponding author: data jz@fake.edu data whatever contact for data it is jzimmer1@uvm.edu. and also it is jwzimmer1990@gmail.com",15))
+print(find_likely("corresponding author: data jz@fake.edu data whatever go to www.our-stuff.com and our repo is https://this.net.why",
+                        20, link_pattern, link_flag_words))
